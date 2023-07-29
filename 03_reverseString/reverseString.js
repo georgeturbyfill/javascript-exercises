36c2b6fe2e745f8a3ccc7c81ff1831aea2849d2e
George Turbyfill <georgeturbyfill@gmail.com>
2023-07-29 12:49:43 -0700

    Complete and test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+// const reverseString = function(str) {
+//   return str.split('').reverse().join('');
+// };
 
-};
+// OR the following code below:
+
+const reverseString = function(str) {
+  let newString = '';
+  for (i = str.length -1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
