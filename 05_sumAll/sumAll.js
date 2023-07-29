da5047916eec27529852fbed7838c02325c8da4f
George Turbyfill <georgeturbyfill@gmail.com>
2023-07-29 15:22:23 -0700

    Complete and Test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let finalSum = 0;
+  if (Number.isInteger(start) == false || Number.isInteger(end) == false) {
+    return 'ERROR';
+  } else if ((start < 0) || (end < 0)){
+    return 'ERROR';
+  } else if (start > end) {
+    for (i = start; i >= end; i--) {
+      finalSum += i;
+    }
+  } else if (start < end) {
+    for (i = start; i <= end; i++) {
+      finalSum += i;
+    }
+  }
+  return finalSum;
 
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
